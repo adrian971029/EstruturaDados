947694b046ecc68871820c14c307c050f51e7a92
Adrian Del Toro <adrian.pena@solinftec.com.br>
2019-05-31 20:08:19 -0300

    Criado layout principal

--- app/src/main/java/com/adrian971029/estruturadados/MainActivity.java
+++ app/src/main/java/com/adrian971029/estruturadados/MainActivity.java
@@ -1,14 +1,35 @@
 package com.adrian971029.estruturadados;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
     }
+
+    @OnClick(R.id.button_add_processos)
+    void onActionAddButtonProcessos() {
+    }
+
 }
